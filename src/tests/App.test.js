03fdb13711d0f7494b1f0b8e4ea2d8eba06817a9
Vinicius Kaminski <vikaminski@hotmail.com>
2022-08-16 15:50:49 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,48 @@
-test('Testando App.js', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import NotFound from '../pages/NotFound';
+
+test('Testando os Links App.js', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  const pokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(pokemonsLink).toBeInTheDocument();
+});
+
+test('Testando Redirecionamento do Home App.js', () => {
+  const { history } = renderWithRouter(<App />);
+  const homeLinkClick = screen.getByRole('link', { name: 'Home' });
+  userEvent.click(homeLinkClick);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('Testando Redirecionamento do About App.js', () => {
+  const { history } = renderWithRouter(<App />);
+  const aboutLinkClick = screen.getByRole('link', { name: 'About' });
+  userEvent.click(aboutLinkClick);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('Testando Redirecionamento do Favorites App.js', () => {
+  const { history } = renderWithRouter(<App />);
+  const favoriteLinkClick = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(favoriteLinkClick);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('Testando Redirecionamento do NotFound App.js', () => {
+  const { history } = renderWithRouter(<NotFound />);
+  history.push('/que-nao-existe');
+  const notFoundPatch = screen.getByText('Page requested not found');
+  expect(notFoundPatch).toBeInTheDocument();
+});
