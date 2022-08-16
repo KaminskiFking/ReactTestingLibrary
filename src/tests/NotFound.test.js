2530d3a718b34e1baa62899a41320bb5987c105b
Vinicius Kaminski <vikaminski@hotmail.com>
2022-08-16 16:51:07 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Teste se contém um heading h2 com o texto Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+  const textHeading = screen.getByRole('heading', { level: 2,
+    name: /Page requested not found/i });
+  expect(textHeading).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem', () => {
+  renderWithRouter(<NotFound />);
+  const image = screen
+    .getByAltText('Pikachu crying because the page requested was not found');
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
